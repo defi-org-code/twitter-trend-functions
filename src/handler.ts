b3ffd16eb43faefc9341b81db167840b51245863
Rotem Yakir <yakir.rotem@gmail.com>
2021-06-29 11:51:48 +0300

    function working for writing and reading

--- src/handler.ts
+++ src/handler.ts
@@ -1,26 +1,209 @@
 import path from "path";
+import { getRecentTweets } from "./twitter-api";
+import { EntitiesResult, Entity, EntityType, Tweet } from "./types";
+import sqlite3, { Database } from "better-sqlite3";
 import fs from "fs-extra";
-import fetch from "node-fetch";
 
-const storage = path.resolve(process.env.HOME_DIR!!, "storage.json");
+const TOP_ENTITIES_PATH = path.resolve(process.env.HOME_DIR!!, "top-entities.json");
+const DB_PATH = path.resolve(process.env.HOME_DIR!!, "twitter.db");
 
-// handlers
+let db: Database;
+
+const ensureDBIsReady = () => {
+  if (!db) {
+    db = sqlite3(DB_PATH);
+
+    db.exec("CREATE TABLE IF NOT EXISTS tweets (id TEXT PRIMARY KEY, count INTEGER)");
+    db.exec(
+      "CREATE TABLE IF NOT EXISTS entities (name TEXT, type INTEGER, count INTEGER, processed INTEGER, lastUpdateTime TEXT, PRIMARY KEY (name, type))"
+    );
+  }
+};
+
+// ############ READER #############
 
 async function _reader(event: any, context: any) {
-  const param = event.pathParameters.param;
-  console.log(`reader running with param = ${param}`);
-  const { timestamp, writeIP } = await fs.readJson(storage);
-  const readIP = (await fetchJson()).origin;
-  return success({ param, timestamp: new Date(timestamp), writeIP, readIP, event, context });
+  return success(await fs.readJson(TOP_ENTITIES_PATH));
 }
 
-async function _writer(event: any, context: any) {
-  await fs.ensureFile(storage);
-  const timestamp = new Date().getTime();
-  const writeIP = (await fetchJson()).origin;
-  await fs.writeJson(storage, { timestamp, writeIP });
+// ############ WRITER #############
+
+const _writer = async (event: any, context: any) => {
+  console.log("---- Fetching recent tweets ----");
+  const response = await getRecentTweets();
+
+  const tweets: Array<Tweet> = response.includes.tweets.map((t: any): Tweet => {
+    return { id: t.id, entities: t.entities, public_metrics: t.public_metrics, counterToUpdate: 0 };
+  });
+
+  console.log("---- Updating tweets ----");
+  for (const t of tweets) {
+    t.counterToUpdate = await updateTweet(t);
+  }
+
+  console.log("---- Processing entities ----");
+  await setProcessedEntities();
+
+  console.log("---- Update entities ----");
+  await updateEntities(tweets);
+
+  console.log("---- Writing result ----");
+  await writeTopEntitiesToDisk();
+
   return success("OK");
-}
+};
+
+const updateTweet = async (tweet: Tweet) => {
+  let count = tweet.public_metrics.retweet_count + tweet.public_metrics.quote_count;
+
+  const prev: any = db.prepare("select * from tweets where id = ?").get(tweet.id);
+
+  if (!prev) {
+    db.prepare("insert into tweets values (?,?)").run(tweet.id, count);
+    return count;
+  } else {
+    db.prepare("update tweets set count = ? where id = ?").run(count, tweet.id);
+    return count - prev.count;
+  }
+};
+
+const setProcessedEntities = async () => {
+  return db
+    .prepare("update entities set processed = count, lastUpdateTime = ? where not IFNULL(processed, -1) = count")
+    .run(new Date().toUTCString());
+};
+
+const updateEntities = async (tweets: Array<Tweet>) => {
+  const time = new Date();
+
+  const entities: Array<Entity> = [];
+
+  tweets.forEach((t: Tweet) => {
+    if (t.entities.cashtags) {
+      t.entities.cashtags.forEach((cashtag) => {
+        const entity = entities.find((e) => e.name === cashtag.tag && e.type === EntityType.CASHHASH);
+
+        if (entity) {
+          entity.count += t.counterToUpdate;
+        } else {
+          entities.push({
+            type: EntityType.CASHHASH,
+            name: cashtag.tag,
+            count: t.counterToUpdate,
+            lastUpdateTime: time,
+          });
+        }
+      });
+    }
+
+    if (t.entities.hashtags) {
+      t.entities.hashtags.forEach((hashtag) => {
+        const entity = entities.find((e) => e.name === hashtag.tag && e.type === EntityType.HASHTAG);
+
+        if (entity) {
+          entity.count += t.counterToUpdate;
+        } else {
+          entities.push({
+            type: EntityType.HASHTAG,
+            name: hashtag.tag,
+            count: t.counterToUpdate,
+            lastUpdateTime: time,
+          });
+        }
+      });
+    }
+
+    if (t.entities.mentions) {
+      t.entities.mentions.forEach((mention) => {
+        const entity = entities.find((e) => e.name === mention.username && e.type === EntityType.MENTION);
+
+        if (entity) {
+          entity.count += t.counterToUpdate;
+        } else {
+          entities.push({
+            type: EntityType.MENTION,
+            name: mention.username,
+            count: t.counterToUpdate,
+            lastUpdateTime: time,
+          });
+        }
+      });
+    }
+
+    if (t.entities.urls) {
+      t.entities.urls.forEach((url) => {
+        const entity = entities.find((e) => e.name === url.url && e.type === EntityType.URL);
+
+        if (entity) {
+          entity.count += t.counterToUpdate;
+        } else {
+          entities.push({
+            type: EntityType.URL,
+            name: url.url,
+            count: t.counterToUpdate,
+            lastUpdateTime: time,
+          });
+        }
+      });
+    }
+  });
+
+  const entitiesStatement = db.prepare(
+    "Insert INTO entities(type,name,count,lastUpdateTime) values (?,?,?,?)\n" +
+      "ON CONFLICT (type,name) DO UPDATE SET count = count + ?, lastUpdateTime = ?"
+  );
+
+  db.transaction((entities: Array<Entity>) => {
+    entities.forEach((entity) => {
+      entitiesStatement.run(
+        entity.type,
+        entity.name,
+        entity.count,
+        entity.lastUpdateTime.toUTCString(),
+        entity.count,
+        entity.lastUpdateTime.toUTCString()
+      );
+    });
+  })(entities);
+
+  console.log("done updateEntities");
+};
+
+const writeTopEntitiesToDisk = async () => {
+  const topEntities = await fetchTopEntities();
+
+  await fs.writeJson(TOP_ENTITIES_PATH, JSON.stringify(topEntities));
+};
+
+const fetchTopEntities = async (): Promise<EntitiesResult> => {
+  const hashtags = db
+    .prepare(
+      "select processed, count, name, lastUpdateTime from entities where type = ? order by processed desc, count desc limit 100"
+    )
+    .all(EntityType.HASHTAG);
+  const cashtags = db
+    .prepare(
+      "select processed, count, name, lastUpdateTime from entities where type = ? order by processed desc, count desc limit 100"
+    )
+    .all(EntityType.CASHHASH);
+  const mentions = db
+    .prepare(
+      "select processed, count, name, lastUpdateTime from entities where type = ? order by processed desc, count desc limit 100"
+    )
+    .all(EntityType.MENTION);
+  const urls = db
+    .prepare(
+      "select processed, count, name, lastUpdateTime from entities where type = ? order by processed desc, count desc limit 100"
+    )
+    .all(EntityType.URL);
+
+  return {
+    hashtags,
+    cashtags,
+    mentions,
+    urls,
+  };
+};
 
 // wrapper
 
@@ -29,6 +212,11 @@ function success(result: any) {
     statusCode: 200,
     body: JSON.stringify(result, null, 2),
   };
+}
+
+async function beforeRunningFunc(this: any, event: any, context: any) {
+  ensureDBIsReady();
+  return await this(event, context);
 }
 
 async function catchErrors(this: any, event: any, context: any) {
@@ -53,5 +241,5 @@ async function fetchJson() {
 
 // exports
 
-export const reader = catchErrors.bind(_reader);
-export const writer = catchErrors.bind(_writer);
+export const reader = catchErrors.bind(beforeRunningFunc.bind(_reader));
+export const writer = catchErrors.bind(beforeRunningFunc.bind(_writer));
---
+++ src/twitter-api.ts
@@ -0,0 +1,39 @@
+// Search for Tweets within the past seven days
+// https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
+
+import needle from "needle";
+
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = process.env.BEARER_TOKEN;
+
+export const getRecentTweets = async () => {
+  const todayUTC = new Date();
+  todayUTC.setUTCHours(0, 0, 0, 0);
+
+  // Edit query parameters below
+  // specify a search query, and any additional fields that are required
+  // by default, only the Tweet ID and text fields are returned
+  const params = {
+    query: "(#defi OR #crypto OR #cryptocurrency) is:retweet",
+    start_time: todayUTC.toISOString(),
+    "tweet.fields": "text,public_metrics,entities,referenced_tweets",
+    "user.fields": "description,public_metrics",
+    expansions: "author_id,referenced_tweets.id",
+    max_results: 11,
+  };
+
+  const res = await needle("get", "https://api.twitter.com/2/tweets/search/recent", params, {
+    headers: {
+      "User-Agent": "v2RecentSearchJS",
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (res.body) {
+    return res.body;
+  } else {
+    throw new Error("Unsuccessful request");
+  }
+};
